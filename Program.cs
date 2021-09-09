2e40db9875bd2b2734d8d8c5ac3b40cac7ac4ffa
sethu <sethu@DESKTOP-24CTG5U>
2021-09-09 08:13:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// C# program to check if a given
+// character is vowel or consonant
+using System;
+public class GFG
+{
+
+    // Function to check whether a
+    // character is vowel or not
+    static void vowelOrConsonant(char x)
+    {
+        if (x == 'a' || x == 'e' || x == 'i' ||
+            x == 'o' || x == 'u' || x == 'A' ||
+            x == 'E' || x == 'I' || x == 'O' || x == 'U')
+            Console.WriteLine("Vowel");
+        else
+            Console.WriteLine("Consonant");
+    }
+
+    // Driver code
+    static public void Main()
+    {
+        vowelOrConsonant('Z');
+        vowelOrConsonant('A');
+    }
+}
